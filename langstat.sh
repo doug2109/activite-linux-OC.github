e1c79e004882447db33f53817639b5937035f40e
doug2109 <edouard@echodelta.fr>
2016-02-22 20:20:20 +0100

    1. Initialisation du script bash (début de fichier commenté = infos script) ; 2. Ajout de la condition (if...fi) de test sur le premier argument

---
+++ langstat.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+##################################################################################
+#				langstat.sh					 #
+##################################################################################
+# Ce script fournit des statistiques sur l'utilisation des lettres dans une langue.
+# On fournit en premier argument un dictionnaire de langue et en deuxième argument un nombre définissant la taille des mots à traiter.
+####
+# 1. Le script parcourt le dictionnaire et indique le nombre d'occurences de chaque lettre dans l'ordre décroissant d'utilisation.
+# 2. Lorsque le deuxième argument est présent et est valide (nombre réel compris entre 0 et la taille du mot le plus long), le script effectue l'action indiquée précédemment (§1) seulement sur les mots de la taille indiquée.
+####
+# Auteur: Édouard LUMET
+# Powered by: OpenClassrooms
+# Version: 0.1-alpha
+##################################################################################
+#
+#### Variables ####
+
+###################
+#
+if [ $1 != "" ] && [ -ef "$1" ]
+then
+	echo "Analyse statistique en cours sur le fichier $1"
+else
+	echo "Veuillez indiquer au moins un argument valide, soit un fichier existant."
+fi
