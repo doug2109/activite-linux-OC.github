bf267006422fad7d0c97a4e286867bd045fa4efe
doug2109 <edouard@echodelta.fr>
2016-02-23 11:05:21 +0100

    --langstat.sh--  1. Suprression des varaibles -contentFile- et -oldFIS-     2. Ajout des variables de conteurs de lettre (expressions -let-) et -tempCountFile-     3. Ajout de la condition et des commandes pour CASE     4. Reste à ajouter l'écriture des résultats sous le bon formzat

--- langstat.sh
+++ langstat.sh
@@ -9,16 +9,41 @@
 # 1. Le script parcourt le dictionnaire et indique le nombre d'occurences de chaque lettre dans l'ordre décroissant d'utilisation.
 # 2. Lorsque le deuxième argument est présent et est valide (nombre réel compris entre 0 et la taille du mot le plus long), le script effectue l'action indiquée précédemment (§1) seulement sur les mots de la taille indiquée.
 ####
-# Auteur: Édouard LUMET
-# Powered by: OpenClassrooms
+# Auteur: Édouard LUMET <edouard[_AT_]echodelta[_DOT_]fr>
+# Powered by: OpenClassrooms <https://openclassrooms.fr>
 # Version: 0.1-alpha
 ##################################################################################
 #
 #### Variables ####
-#contentFile="cat $1"
-oldIFS=$IFS
 IFS=$'\n'
 filePath=$1
+let "a = 0"
+let "b = 0"
+let "c = 0"
+let "d = 0"
+let "e = 0"
+let "f = 0"
+let "g = 0"
+let "h = 0"
+let "i = 0"
+let "j = 0"
+let "k = 0"
+let "l = 0"
+let "m = 0"
+let "n = 0"
+let "o = 0"
+let "p = 0"
+let "q = 0"
+let "r = 0"
+let "s = 0"
+let "t = 0"
+let "u = 0"
+let "v = 0"
+let "w = 0"
+let "x = 0"
+let "y = 0"
+let "z = 0"
+tempCountFile="count.txt.tmp"
 ###################
 #
 if [ $1 != "" ] && [ -e $filePath ]
@@ -27,11 +52,91 @@ then
 	sleep 5
 	for ligne in $(<$filePath)
 	do
+		IFS=$''
 		for lettre in $ligne
 		do
-			case
+			case lettre in
+				"A")
+					let "a = $a + 1"
+					;;
+				"B")
+					let "b = $b + 1"
+					;;
+				"C")
+					let "c = $c + 1"
+					;;
+				"D")
+					let "d = $d + 1"
+					;;
+				"E")
+					let "e = $e + 1"
+					;;
+				"F")
+					let "f = $f + 1"
+					;;
+				"G")
+					let "g = $g + 1"
+					;;
+				"H")
+					let "h = $h + 1"
+					;;
+				"I")
+					let "i = $i + 1"
+					;;
+				"J")
+					let "j = $j + 1"
+					;;
+				"K")
+					let "k = $k + 1"
+					;;
+				"L")
+					let "l = $l + 1"
+					;;
+				"M")
+					let "m = $m + 1"
+					;;
+				"N")
+					let "n = $n + 1"
+					;;
+				"O")
+					let "o = $o + 1"
+					;;
+				"P")
+					let "p = $p + 1"
+					;;
+				"Q")
+					let "q = $q + 1"
+					;;
+				"R")
+					let "r = $r + 1"
+					;;
+				"S")
+					let "s = $s + 1"
+					;;
+				"T")
+					let "t = $t + 1"
+					;;
+				"U")
+					let "u = $u + 1"
+					;;
+				"V")
+					let "v = $v + 1"
+					;;
+				"W")
+					let "w = $w + 1"
+					;;
+				"X")
+					let "x = $x + 1"
+					;;
+				"Y")
+					let "y = $y + 1"
+					;;
+				"Z")
+					let "z = $z + 1"
+					;;
 			esac
 		done
+		IFS=$'\n'
 	done
 else
 	echo "Veuillez indiquer au moins un argument valide, soit un fichier existant."
